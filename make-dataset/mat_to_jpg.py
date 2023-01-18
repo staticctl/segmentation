567a0023d1b2f6fd47291770950ddcddc4a0f22e
17306371927 <2669697029@qq.com>
2023-01-18 22:01:07 +0800

    files

--- make-dataset/mat_to_jpg.py
+++ make-dataset/mat_to_jpg.py
@@ -4,11 +4,15 @@ import matplotlib.pyplot as plt
 import os
 import scipy.io as scio
 from PIL import Image
+
+
 def get_all_files():
     path = os.path.abspath('.')
     # print(path)
     path1 = path + '/groundTruth'
     return os.listdir(path1)  # 打印IELTS文件夹内的文件名称
+
+
 def mat_to_jpg(files_list):
     for i in range(len(files_list)):
         path = r'./groundTruth/{}'.format(files_list[i])
@@ -16,10 +20,20 @@ def mat_to_jpg(files_list):
         # print(mat_data)
         array_mat = mat_data['groundTruth'][0][0]['Segmentation']
         array_mat = np.asarray(array_mat)
+        array_mat = array_to_binary(array_mat)
         image = Image.fromarray(array_mat).convert("L")  # L为模式
         split_list = files_list[i].split('.')
-        image.save("./groundTruth_jpg/{}.jpg".format(split_list[0]))  # 输出图片格式可以自己选择
+        image.save("./groundTruth_jpg/{}_mask.jpg".format(split_list[0]))  # 输出图片格式可以自己选择
 
+
+def array_to_binary(array):
+    for i in range(len(array)):
+        for j in range(len(array[0])):
+            if array[i][j] == 1:
+                array[i][j] = 0
+            else:
+                array[i][j] = 255
+    return array
 
 if __name__ == '__main__':
     files_list = get_all_files()
