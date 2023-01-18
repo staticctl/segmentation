e4f9a1d931a5038609e2e794240286087b64d60b
17306371927 <2669697029@qq.com>
2023-01-18 20:24:53 +0800

    files

--- main.py
+++ main.py
@@ -1,16 +1,84 @@
-# This is a sample Python script.
+#main.py
+import numpy as np
+import torch
+import argparse
+from torch.utils.data import DataLoader
+from torch import autograd, optim
+from torchvision.transforms import transforms
+from unet import UNet
+from dataset import MyDataset
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# 是否使用cuda
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+x_transforms = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize([0.5], [0.5])  # 复活了，这里修改就没错误了
+])
+
+# mask只需要转换为tensor
+y_transforms = transforms.ToTensor()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def train_model(model, criterion, optimizer, dataload, num_epochs=10):
+    for epoch in range(0,num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+        dt_size = len(dataload.dataset)
+        epoch_loss = 0
+        step = 0
+        for x, y in dataload:
+            step += 1
+            inputs = x.to(device)
+            labels = y.to(device)
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            # forward
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            epoch_loss += loss.item()
+            print("%d/%d,train_loss:%0.3f" %
+                  (step,
+                   (dt_size - 1) // dataload.batch_size + 1, loss.item()))
+        print("epoch %d loss:%0.3f" % (epoch, epoch_loss))
+    torch.save(model.cpu().state_dict(), 'weights_%d.pth' % epoch)
+    return model
 
 
-# Press the green button in the gutter to run the script.
+#训练模型
+def train():
+    batch_size = 1
+    liver_dataset = MyDataset(
+        "image/train/data", "image/train/gt",transform=x_transforms, target_transform=y_transforms)
+    dataloaders = DataLoader(
+        liver_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+    train_model(model, criterion, optimizer, dataloaders)
+
+
+#显示模型的输出结果
+def test():
+    liver_dataset = MyDataset(
+        "image/val/data", "image/val/gt", transform=x_transforms, target_transform=y_transforms)
+    dataloaders = DataLoader(liver_dataset, batch_size=1)
+    import matplotlib.pyplot as plt
+    plt.ion()
+    with torch.no_grad():
+        for x, _ in dataloaders:
+            y = model(x)
+            img_y = torch.squeeze(y).numpy()
+            plt.imshow(img_y)
+            plt.pause(0.01)
+        plt.show()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    pretrained = False
+    model = UNet(1, 1).to(device)
+    if pretrained:
+        model.load_state_dict(torch.load('./weights_4.pth'))
+    criterion = torch.nn.BCELoss()
+    optimizer = optim.Adam(model.parameters())
+    train()
+    test()
